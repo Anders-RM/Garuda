6bdc9ba2843b4e713e58daa1fdf868f1bc2a0e7c
Anders-RM <62700152+Anders-RM@users.noreply.github.com>
2023-02-28 18:24:49 +0100

    temp

--- auto.sh
+++ auto.sh
@@ -1,8 +1,12 @@
 #! /bin/zsh
-sudo pacman -Syu --noconfirm
-sudo pacman -Sy git --noconfirm
-sudo pacman -Sy yakuake --noconfirm
-sudo pamac install visual-studio-code-bin --noconfirm
+sudo garuda-update
+sudo yay -S --noconfirm pamac-all
+sudo pacman -Sy --noconfirm git
+sudo pacman -Sy --noconfirm yakuake
+sudo pacman -Rsu --noconfirm octopi
+sudo pamac install --noconfirm visual-studio-code-bin
+sudo pacman -Rsu --noconfirm latte-dock
+sudo pacman -Rsu --noconfirm firefox
 
 #git config
 ssh-keygen -q -t rsa -N '@Ndersraeder' -f ~/.ssh/id_rsa -C "andersrm1808@gmial.com" <<<y >/dev/null 2>&1
@@ -12,7 +16,10 @@ git config --global user.name "anddersrm1808"
 
 mv up-shutdown /usr/bin/
 mv upgrade-all /usr/bin/
+mv open_as_root.desktop ~/.local/share/kservices5/ServiceMenus
 
+sudo lookandfeeltool -a Sweet
+sed -i 's/BorderlessMaximizedWindows=true/BorderlessMaximizedWindows=false/' ~/.config/kwinrc
 
 #logout
 qdbus org.kde.ksmserver /KSMServer org.kde.KSMServerInterface.logout 0 3 3
