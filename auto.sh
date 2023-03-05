c833cc8ddb2a77f1549d7052778d99a1ad686a5f
Anders-RM <62700152+Anders-RM@users.noreply.github.com>
2023-03-06 00:12:38 +0100

    git up

--- auto.sh
+++ auto.sh
@@ -43,9 +43,9 @@ sudo pamac install --no-confirm opera-ffmpeg-codecs
 sudo pamac remove --no-confirm firedragon-extension-plasma-integration firedragon
 
 #git config
-ssh-keygen -q -t rsa -N '@Ndersraeder' -f ~/.ssh/id_rsa -C "andersrm1808@gmial.com" <<<y >/dev/null 2>&1
-git config --global user.email "andersrm1808@gmial.com"
-git config --global user.name "anddersrm1808"
+ssh-keygen -q -t rsa -N '@Ndersraeder' -f ~/.ssh/id_rsa -C "Anders_RMathiesen@pm.me <<<y >/dev/null 2>&1
+git config --global user.email "Anders_RMathiesen@pm.me
+git config --global user.name "Anders-RM"
 
 mkdir -p ~/.local/share/kservices5/ServiceMenus
 mv up-shutdown /usr/bin/
